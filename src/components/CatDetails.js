9be33b9dc4f46d7c69cb051df661b5ac21e08ce5
Gilberto Moraes <moraesdev@gmail.com>
2022-03-09 12:34:01 -0300

    fixes close modal and update packages

--- src/components/CatDetails.js
+++ src/components/CatDetails.js
@@ -1,12 +1,24 @@
+import Badge from 'react-bootstrap/Badge'
 import Image from 'react-bootstrap/Image'
 
+const renderBadget = (description) => (
+    <Badge pill bg="secondary">
+        {description}
+    </Badge>)
+
 const CatDetails = ({ cat }) => {
-    if (!cat) {
-        return null
-    }
     return (
         <>
-            {cat.id}
+            <p>
+                <strong> Code: {cat.id}</strong>
+            </p>
+            <p>
+                <strong> Breads: </strong>
+                {!cat.lenght && (
+                    renderBadget("Mutt")
+                )}
+                {cat.lenght && cat.breeds.map((description) => renderBadget(description))}
+            </p>
             <Image src={cat.url} fluid />
         </>
     )
